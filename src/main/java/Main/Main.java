4d7f5d5e8bf1da7241a188cf115d9f0059397792
Andre Garcia <andregcia@gmail.com>
2014-03-16 20:50:20 +0100

    Choix couleur - erreur ajout couleur

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,28 +1,92 @@
 package Main;
 
 import java.util.Scanner;
+import joueur.Joueur;
+import util.Couleur;
+import java.util.Arrays;
 
 public class Main {
         
     //Début du jeu 
     
-    
 
     public static void main(String[] args) {
         
         //On pose la question du nombre de joueur
+        Scanner sc=new Scanner(System.in);
         int nbJoueur = 0;
         while(nbJoueur < 2 || nbJoueur > 4){
-            Scanner sc=new Scanner(System.in);
-            System.out.println("Combien de joueur êtes-vous ? ");
+            
+            System.out.print("Combien de joueur êtes-vous ? ");
             String saisieNb=sc.nextLine();
             
             if(saisieNb.length() > 0){
-                nbJoueur = Integer.parseInt(saisieNb); 
+                try{
+                    nbJoueur = Integer.parseInt(saisieNb); 
+                }catch(NumberFormatException e){
+                    System.out.println("Ceci n'est pas un nombre.");
+                }
             }else{
                 System.out.println("Il faut saisir un nombre.");
+            }            
+        }
+        
+        //Choix de la couleur pour chaque joueur
+        Joueur[] tabJoueur = new Joueur[nbJoueur-1];
+        
+        for (int i=0;i<nbJoueur;i++){
+            //On présente la liste des couleurs
+            boolean colorExist = false;
+            int nbCouleur = 1;
+            Couleur colorJoueur = null;
+            while(!colorExist){
+                System.out.print("Choisir une couleur : ");
+                for(Couleur color : Couleur.values()){
+                    System.out.print(nbCouleur + "-" + color.toString() + " ");
+                    nbCouleur++;
+                }
+                System.out.println("");
+                System.out.print("Mon choix : ");
+                String saisieCouleur=sc.nextLine();
+                
+                if(saisieCouleur.length() > 0){
+                    try{
+                        nbCouleur = Integer.parseInt(saisieCouleur);
+                        System.out.println(nbCouleur);                                
+                    }catch(NumberFormatException e){
+                        System.out.println("Ceci n'est pas un nombre.");
+                    }
+                }else{
+                    System.out.println("Il faut saisir le nombre correspondant à la couleur de votre choix.");
+                }   
+                
+                switch(nbCouleur){
+                    case 1:
+                        colorJoueur = colorJoueur.VERT;
+                        colorExist = true;
+                        break;
+                    case 2:
+                        colorJoueur = colorJoueur.BLEU;
+                        colorExist = true;
+                        break;
+                    case 3:
+                        colorJoueur = colorJoueur.ROUGE;
+                        colorExist = true;
+                        break;
+                    case 4:
+                        colorJoueur = colorJoueur.JAUNE;
+                        colorExist = true;
+                        break;
+                    default: 
+                        colorExist = false;
+                        nbCouleur = 1;
+                        System.out.println("Le nombre choisi n'existe pas.");
+                        break;
+                }
             }
-                      
+            
+            //On attribut la couleur au joueur
+            tabJoueur[i] = new Joueur(colorJoueur);
         }
     }
 }
