92e8da592928c995d9fdc1a71c86c356d822b1d7
Andre Garcia <andregcia@gmail.com>
2014-03-17 11:00:56 +0100

    Création des personnages dans le main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 import joueur.Joueur;
 import util.Couleur;
 import java.util.ArrayList;
+import pion.Personnage;
 
 public class Main {
         
@@ -98,7 +99,35 @@ public class Main {
             }
             
             //On attribut la couleur au joueur
-            listJoueur.add(new Joueur(colorJoueur));
+            Joueur jtemp = new Joueur(colorJoueur);
+            //On créer les pions personnages
+            Personnage pers;
+            ArrayList<Personnage> listPers = new ArrayList<Personnage>();
+            for(int j=1;j<=7;j++){
+                if(j<=3){
+                    pers = new Personnage(jtemp.getCouleur(),1);
+                    listPers.add(pers);
+                }
+                if(j>3 && j<=5){
+                    pers = new Personnage(jtemp.getCouleur(),2);
+                    listPers.add(pers);
+                }
+                if(j>5 && j<=7){
+                    pers = new Personnage(jtemp.getCouleur(),3);
+                    listPers.add(pers);
+                }                 
+            }
+            pers = new Personnage(jtemp.getCouleur(),4);
+            listPers.add(pers);
+            pers = new Personnage(jtemp.getCouleur(),5);
+            listPers.add(pers);
+            pers = new Personnage(jtemp.getCouleur(),6);
+            listPers.add(pers);
+            //On ajoute le joueur dans la liste
+            //jtemp.setPions(listPers); //ici on affect au joueur en cours sa liste de pions personnages. ATTTENTION : incompatible type !
+            listJoueur.add(jtemp);
+            
+            //System.out.println(listJoueur.toString());
         }
     }
 }
